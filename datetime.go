99600f011b1cf698aa419632edf2ebda3165e6f5
mtfelian <atmajotii@mihteh.com>
2015-11-19 11:47:28 +0300

    Исправил стилистику комментариев

--- datetime.go
+++ datetime.go
@@ -54,13 +54,14 @@ func ToDate(t time.Time) Date {
 }
 
 // DaysBefore возвращает количество полных дней, прошедших от d до endDate
-// Если d было вчера, endDate - сегодня, то возвращается 1
+// Например, если d было вчера, endDate - сегодня, то возвращается 1
 // Если endDate было раньше чем d, то возвращается отрицательное число.
 func (d Date) DaysBefore(endDate Date) int {
 	return int(endDate.Time.Sub(d.Time).Hours() / 24)
 }
 
-// StringToDateTime преобразует строку по стандартному шаблону даты-времени в дату-время
+// StringToDateTime формирует объект типа DateTime на основе строки s,
+// заданной по шаблону DateTimeLayout
 func StringToDateTime(s string) (DateTime, error) {
 	t, err := time.ParseInLocation(DateTimeLayout, s, defaultLocation)
 	if err != nil {
@@ -69,8 +70,9 @@ func StringToDateTime(s string) (DateTime, error) {
 	return ToDateTime(t), nil
 }
 
-// StringDateToDateTimeHMS преобразует строку по стандартному шаблону
-// даты в дату-время с заданным значением часов, минут и секунд
+// StringDateToDateTimeHMS формирует объект типа DateTime на основе строки s,
+// заданной по шаблону DateTimeLayout, и значений часов, минут и секунд,
+// заданных параметрами hours, mins, secs соответственно.
 func StringDateToDateTimeHMS(s string, hours int, mins int, secs int) (DateTime, error) {
 	t, err := time.ParseInLocation(DateLayout, s, defaultLocation)
 	if err != nil {
@@ -81,7 +83,7 @@ func StringDateToDateTimeHMS(s string, hours int, mins int, secs int) (DateTime,
 	return d, nil
 }
 
-// StringToDate преобразует строку по стандартному шаблону даты в дату
+// StringToDate формирует объект типа Date на основе строки s, заданной по шаблону DateLayout
 func StringToDate(s string) (Date, error) {
 	t, err := time.ParseInLocation(DateLayout, s, defaultLocation)
 	if err != nil {
@@ -90,51 +92,55 @@ func StringToDate(s string) (Date, error) {
 	return ToDate(t), nil
 }
 
-// NeverDate возвращает дату в далёком прошлом
+// NeverDate возвращает объект Date, соответствующий дате в далёком прошлом
 func NeverDate() Date {
 	t, _ := time.ParseInLocation(DateLayout, "1990-01-01", defaultLocation)
 	return ToDate(t)
 }
 
-// DateNow возвращает дату сегодня
+// DateNow возвращает объект Date, соответствующий дате сегодня
 func DateNow() Date {
 	return ToDate(time.Now())
 }
 
-// DateTimeNow возвращает дату-время сейчас
+// DateTimeNow возвращает объект DateTime, соответствующий дате-времени сейчас
 func DateTimeNow() DateTime {
 	return ToDateTime(time.Now())
 }
 
-// DateTimeTodayHMS возвращает дату-время сегодня в заданными значениями
-// часов, минут, секунд
+// DateTimeTodayHMS возвращает объект DateTime, соответствующий дате сегодня,
+// с установленными значениями часов, минут, секунд согласно заданным параметрам
+// hours, mins, secs соответственно.
 func DateTimeTodayHMS(hours int, mins int, secs int) DateTime {
 	d := ToDateTime(time.Now())
 	return d.SetHMS(hours, mins, secs)
 }
 
-// NeverTime возвращает дату-время в далёком прошлом
+// NeverTime возвращает объект DateTime, соответствующий дате-времени в далёком прошлом
 func NeverTime() DateTime {
 	t, _ := time.ParseInLocation(DateTimeLayout, "1990-01-01 00:00:00", defaultLocation)
 	return ToDateTime(t)
 }
 
-// setDefaultLayoutIfEmpty устанавливает шаблон вывода даты-времени
-// по умолчанию, если шаблон не установлен
+// setDefaultLayoutIfEmpty устанавливает в объекте DateTime шаблон вывода даты-времени
+// по умолчанию DateTimeLayout, если шаблон не установлен
 func (d *DateTime) setDefaultLayoutIfEmpty() {
 	if strings.TrimSpace(d.Layout) == "" {
 		d.Layout = DateTimeLayout
 	}
 }
 
-// setDefaultLayoutIfEmpty устанавливает шаблон вывода даты по умолчанию, если шаблон не установлен
+// setDefaultLayoutIfEmpty устанавливает в объекте Date шаблон вывода даты
+// по умолчанию DateLayout, если шаблон не установлен
 func (d *Date) setDefaultLayoutIfEmpty() {
 	if strings.TrimSpace(d.Layout) == "" {
 		d.Layout = DateLayout
 	}
 }
 
-// SetHMS устанавливает значения часов, минут и секунд
+// SetHMS возвращает новый объект DateTime на основе объекта d,
+// с заданными значениями часов, минут, секунд в параметрах
+// hours, mins, secs соответственно.
 func (d DateTime) SetHMS(hours int, mins int, secs int) DateTime {
 	t := d.Time
 	dS := fmt.Sprintf("%s %02d:%02d:%02d", t.Format(DateLayout), hours, mins, secs)
@@ -143,14 +149,15 @@ func (d DateTime) SetHMS(hours int, mins int, secs int) DateTime {
 	return d
 }
 
-// ConvertToDate преобразует дату-время в дату
+// ConvertToDate преобразует объект DateTime в объект Date
 func (d DateTime) ConvertToDate() Date {
 	dS := d.Time.Format(DateLayout)
 	t, _ := time.ParseInLocation(DateLayout, dS, defaultLocation)
 	return ToDate(t)
 }
 
-// ConvertToDateTimeHMS преобразует дату в дату-время с заданными значениями часов, минут и секунд
+// ConvertToDateTimeHMS преобразует объект Date в объект DateTime
+// с учётом заданных часов, минут, секунд в параметрах hours, mind, secs соответственно.
 func (d Date) ConvertToDateTimeHMS(hours int, mins int, secs int) DateTime {
 	dt := DateTime{
 		Time:   d.Time,
@@ -162,29 +169,29 @@ func (d Date) ConvertToDateTimeHMS(hours int, mins int, secs int) DateTime {
 }
 
 // After возвращает true если дата d позднее d1, иначе false
-// Сравнение с точностью до дня.
+// Сравнение происходит с точностью до дня.
 func (d Date) After(d1 Date) bool {
 	return d.Time.After(d1.Time)
 }
 
 // Before возвращает true если дата d ранее d1, иначе false
-// Сравнение с точностью до дня.
+// Сравнение происходит с точностью до дня.
 func (d Date) Before(d1 Date) bool {
 	return d.Time.Before(d1.Time)
 }
 
 // Between возвращает true если дата d находится в интервале дат (d1; d2), иначе false
-// Сравнение с точностью до дня.
+// Сравнение происходит с точностью до дня.
 func (d Date) Between(d1, d2 Date) bool {
 	return d.After(d1) && d.Before(d2)
 }
 
-// After возвращает true если дата-время obj позднее d1, иначе false
+// After возвращает true если дата-время d позднее d1, иначе false
 func (d DateTime) After(d1 DateTime) bool {
 	return d.Time.After(d1.Time)
 }
 
-// Before возвращает true если дата-время obj ранее d1, иначе false
+// Before возвращает true если дата-время d ранее d1, иначе false
 func (d DateTime) Before(d1 DateTime) bool {
 	return d.Time.Before(d1.Time)
 }
@@ -195,7 +202,8 @@ func (d DateTime) Between(d1, d2 DateTime) bool {
 	return d.After(d1) && d.Before(d2)
 }
 
-// UnmarshalJSON - правило преобразования поля JSON в объект DateTime
+// UnmarshalJSON - реализует интерфейс json.Unmarshaler для объекта DateTime
+// десериализация происходит с учётом шаблона, заданного в свойстве Layout
 func (d *DateTime) UnmarshalJSON(data []byte) error {
 	d.setDefaultLayoutIfEmpty()
 	var s string
@@ -210,13 +218,15 @@ func (d *DateTime) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
-// MarshalJSON - правило преобразования объекта DateTime в поле JSON
+// MarshalJSON - реализует интерфейс json.Marshaler для объекта DateTime
+// сериализация происходит с учётом шаблона, заданного в свойстве Layout
 func (d DateTime) MarshalJSON() ([]byte, error) {
 	d.setDefaultLayoutIfEmpty()
 	return []byte(strconv.Quote(d.String())), nil
 }
 
-// UnmarshalJSON - правило преобразования поля JSON в объект Date
+// UnmarshalJSON - реализует интерфейс json.Unmarshaler для объекта Date
+// десериализация происходит с учётом шаблона, заданного в свойстве Layout
 func (d *Date) UnmarshalJSON(data []byte) error {
 	d.setDefaultLayoutIfEmpty()
 	var s string
@@ -231,25 +241,26 @@ func (d *Date) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
-// MarshalJSON - правило преобразования объекта Date в поле JSON
+// MarshalJSON - реализует интерфейс json.Marshaler для объекта Date
+// сериализация происходит с учётом шаблона, заданного в свойстве Layout
 func (d Date) MarshalJSON() ([]byte, error) {
 	d.setDefaultLayoutIfEmpty()
 	return []byte(strconv.Quote(d.String())), nil
 }
 
-// String преобразует объект DateTime в строку согласно заданного шаблона
+// String преобразует объект DateTime в строку согласно шаблона в свойстве Layout
 func (d DateTime) String() string {
 	d.setDefaultLayoutIfEmpty()
 	return d.Time.Format(d.Layout)
 }
 
-// String преобразует объект Date в строку согласно заданного шаблона
+// String преобразует объект Date в строку согласно шаблона в свойстве Layout
 func (d Date) String() string {
 	d.setDefaultLayoutIfEmpty()
 	return d.Time.Format(d.Layout)
 }
 
-func scanInternal(value interface{}) (time.Time, error) {
+func scan(value interface{}) (time.Time, error) {
 	t := time.Time{}
 	if value == nil {
 		return t, nil
@@ -264,7 +275,7 @@ func scanInternal(value interface{}) (time.Time, error) {
 // Scan преобразует значение времени в БД к типу DateTime
 func (d *DateTime) Scan(value interface{}) error {
 	d.setDefaultLayoutIfEmpty()
-	t, err := scanInternal(value)
+	t, err := scan(value)
 	if err != nil {
 		return err
 	}
